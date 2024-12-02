08df76f7fd97766c815e6bed37aabe697072fc60
AliyanAkmal <aliyanakmal2002@gmail.com>
2024-12-02 17:15:14 +0500

    atubiye

---
+++ src/pages/roadMap/data.js
@@ -0,0 +1,38 @@
+const roadmap = [
+  {
+    title: "$1M Market Cap",
+    details:
+      "Welcome to the most uneventful crypto launch of all time. No hype, no drama, just a simple coin and a shovel.",
+  },
+  {
+    title: "$5M Market Cap",
+    details: "Launch the 'Super Dull' NFT Collection. Boring, non-unique NFTs.",
+  },
+  {
+    title: "$10M Market Cap",
+    details: "Shovel Logo Upgrade: Now with an actual shovel.",
+  },
+  {
+    title: "$25M Market Cap",
+    details: "Boring Coin Community Meetup: Join us for an hour of nothing.",
+  },
+  {
+    title: "$50M Market Cap",
+    details:
+      "Meme the Moon... Or Not: Virtual shovel party and meme competition.",
+  },
+  {
+    title: "$100M Market Cap",
+    details: "24/7 Live Stream of Nothing: Just a shovel, juice box, and logo.",
+  },
+  {
+    title: "$250M Market Cap",
+    details:
+      "World Domination (of Boredom): Experience our boring virtual tunnel.",
+  },
+  {
+    title: "$1B Market Cap",
+    details: "Boring Coin Global Impact: Campaign to spread boredom globally.",
+  },
+];
+export default roadmap;
