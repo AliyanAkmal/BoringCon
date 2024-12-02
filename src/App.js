1b5f13d3ec4125969bc3cdf81ea95a01940c7171
SaqibAmin369 <saqib.hyd369@gmail.com>
2024-12-02 22:08:57 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { Box } from "@mui/material";
-import React from "react";
-import backImage from "./assets/kennedytheairflow_An_underground_cartoon_tunnel_system_sprawlin (1).png";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/route";
+import { Box } from '@mui/material';
+import React from 'react';
+import backImage from './assets/kennedytheairflow_An_underground_cartoon_tunnel_system_sprawlin (1).png';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/route';
 
 const App = () => {
   return (
     <Box
       sx={{
         backgroundImage: `url('${backImage}')`,
-<<<<<<< HEAD
-        padding: "0rem 4rem",
-        height: "100vh",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-=======
         padding: '0rem 4rem',
-        // height: '100vh',
+        height: '100vh',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
->>>>>>> 0b3c55a (road map)
       }}
     >
       <RouterProvider router={router} />
